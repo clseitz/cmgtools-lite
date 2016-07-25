a28b9e19277cca88d9466960d60c3371e653e988
claudia seitz <clseitz.physics@gmail.com>
2016-07-25 13:28:55 +0200

    committing to fix conflicts

--- SUSYAnalysis/python/tools/eventVars_1l_signal.py
+++ SUSYAnalysis/python/tools/eventVars_1l_signal.py
@@ -13,7 +13,7 @@ xsecFile = "../python/tools/glu_xsecs_13TeV.txt"
 
 cntsSusy = {} # dict for signal counts
 #cntFile = "../python/tools/t1ttt_scan_counts.txt"
-cntFile = "../python/tools/scans/counts_T1tttt_wSkim.txt"
+cntFile = "../python/tools/scans/counts_T1tttt_2016.txt"
 
 def loadSUSYparams():
 
@@ -74,6 +74,7 @@ class EventVars1L_signal:
             ### Masses and Xsec
             'mGo','mLSP','susyXsec',
             'susyNgen','totalNgen','susyWgen',
+            'nISR','nISRweight','nISRweightup', 'nISRweightdown','nISRweightsyst_up', 'nISRweightsyst_down',
             ## LHE Scale Weights
             #("nScaleWgt","I"),("ScaleWgt","I",10,"nScaleWgt")
             ("ScaleWgt","F",maxLHEidx,maxLHEidx)
@@ -118,7 +119,26 @@ class EventVars1L_signal:
 
             # Number of generated events
             #ret['totalNgen'] = cntTotal
+            nISR = 0
+            if hasattr(event,'nIsr'): nISR = event.nIsr
+            nISRforWeights = int(nISR)
+            if nISR > 6:
+                nISRforWeights = 6
+            ret['nISR'] = int(nISR)
+            ISRweights = { 0: 1.0, 1 : 0.882, 2 : 0.792, 3 : 0.702, 4 : 0.648, 5 : 0.601, 6 : 0.515}
+            ISRweightssyst = { 0: 0.0, 1 : 0.059, 2 : 0.104, 3 : 0.149, 4 : 0.176, 5 : 0.199, 6 : 0.242}
 
+            nISRweight = ISRweights[nISRforWeights]
+            nISRweightsyst_up = ISRweights[nISRforWeights]+ISRweightssyst[nISRforWeights]
+            nISRweightsyst_down = ISRweights[nISRforWeights]-ISRweightssyst[nISRforWeights]
+            ret['nISRweight'] = nISRweight
+
+            ret['nISRweightsyst_up'] = nISRweightsyst_up
+            ret['nISRweightsyst_down'] = nISRweightsyst_down
+#            print nISR, nISRweight, nISRweightup, nISRweightdown,nISRweightsyst_up, nISRweightsyst_down
+            ###Get ISR stuff
+
+            ##############
             if (mGo,mLSP) in cntsSusy:
                 #ret['totalNgen'] = cntsSusy[(mGo,mLSP)][0] # merged scan: 93743963
                 if "Scan" in self.sample: ret['totalNgen'] = 93743963
@@ -130,6 +150,7 @@ class EventVars1L_signal:
                 ret['susyNgen'] = 1
                 ret['susyWgen'] = 1
 
+            #print "don't to LHE stuff"
             #### LHE Weights (for Scale uncert) #####
             ## Scale uncertainty
             ## https://indico.cern.ch/event/459797/contribution/2/attachments/1181555/1710844/mcaod-Nov4-2015.pdf
@@ -138,18 +159,21 @@ class EventVars1L_signal:
             ## to nominal scale, indices 5 and 7 correspond to "unphysical"
             ## anti-correlated variations)
 
+            noLHEstuff = True
+            if noLHEstuff:
+                return ret
             global lheDict
 
             if len(lheDict) == 0: loadLHE()
-            #print "1", lheDict.keys()
+            print "1", lheDict.keys()
 
             # initialize dummy list
             scaleWgts = [1 for i in range(0,maxLHEidx)]
             # average weights
             meanWgts  = [1 for i in range(0,maxLHEidx)]
 
-            #print self.sample
-            #print "2", lheDict.keys()
+            print self.sample
+            print "2", lheDict.keys()
 
             sampkey = self.sample
 
@@ -157,7 +181,7 @@ class EventVars1L_signal:
                 # search for gluino mass in keys
                 for point in lheDict.keys():
                     if str(mGo) in point: sampkey = point; break
-            #print sampkey
+            print sampkey
 
             if sampkey in lheDict:
                 meanWgts = lheDict[sampkey]
@@ -173,10 +197,9 @@ class EventVars1L_signal:
                     #print wgt
                     scaleWgts.append(wgt)
 
-                #print scaleWgts
+                print scaleWgts
             else: print "No mean scale weights found for", sampkey
             ret['ScaleWgt'] = scaleWgts
-
         return ret
 
 if __name__ == '__main__':
